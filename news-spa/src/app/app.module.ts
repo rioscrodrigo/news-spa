e5d91b53915a87e0f04b06e822a31695af84dccc
Rodrigo Rios <rios.c.rodrigo@gmail.com>
2023-07-23 00:04:10 -0400

    feat!: Initial Commit

---
+++ news-spa/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NewsComponent } from './news/news.component';
+import { UsersComponent } from './users/users.component';
+
+@NgModule({
+  declarations: [AppComponent, NewsComponent, UsersComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
